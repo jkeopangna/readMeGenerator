ec7277612463f690f2d05f7c80b2c950aac4d224
Jon Keopangna <jkeopangna@gmail.com>
2020-11-18 16:08:41 -0700

    Added question prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,61 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-];
+inquirer
+// array of questions for user
+// const questions =
+.prompt ([
+    {
+        type: 'input',
+        message: 'What is the title of your application?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Description of application:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Installation instructions:',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Usage information:',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Contributions:',
+        name: 'contributions'
+    },
+    {
+        type: 'input',
+        message: 'Testing:',
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: 'Licenses:',
+        choices: ['BSD', 'MIT', 'GPL'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Github:',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'Email:',
+        name: 'email'
+    },
+])
+
+.then((data) => {
+
+console.log(data);
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -14,3 +68,4 @@ function init() {
 
 // function call to initialize program
 init();
+});
